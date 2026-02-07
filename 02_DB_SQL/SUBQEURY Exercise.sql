e3bd2452cf6b29fa05e106adf566e2dc58645224
edbdmss <edbdms@naver.com>
2026-02-07 18:45:09 +0900

    complete SUBQEURY Exercise

--- 02_DB_SQL/SUBQEURY Exercise.sql
+++ 02_DB_SQL/SUBQEURY Exercise.sql
@@ -48,9 +48,8 @@ where e.sal > (select avg(sal)
 				from emp e2);
 
 
--- 다중행 서브 쿼리(sub query의 결과값이 하나 이상)
+
 -- 6.급여가 3000이상 사원이 소속된 부서에 속한 사원이름, 급여 검색
--- 급여가 3000이상 사원의 부서 번호
 -- in 연산자 안에 있는 서브쿼리
 
 select e.ename, e.sal
@@ -59,21 +58,17 @@ where e.deptno in (select e2.deptno
 				   from emp e2
                    where e2.sal >= 3000);
 
+
 -- 7. in 연산자를 이용하여 부서별로 가장 급여를 많이 받는 사원의 정보(사번, 사원명, 급여, 부서번호) 검색
 -- 사번, 사원명, 급여, 부서번호 
+-- 결과를 부서번호 내림차순 정렬
 select e.empno, e.ename, e.sal, e.deptno
 from emp e
 where (e.deptno, e.sal) in (select e2.deptno, max(e2.sal)
-					from emp e2
-                    group by e2.deptno)
+							from emp e2
+							group by e2.deptno)
 order by e.deptno desc;
                     
-                    
--- ? 부서별로 가장 급여를 많이 받는 사원
-	
-    
-
--- ? 결과를 부서번호 내림차순 정렬
 
 
 -- 8. 직급(job)이 MANAGER인 사람이 속한 부서의 부서 번호와 부서명(dname)과 지역검색(loc)
@@ -86,7 +81,7 @@ where d.deptno in (select e.deptno
 
 -- 실습
 -- 모든 사원의 이름, 급여, 그리고 회사 전체 평균 급여를 나란히 출력
--- 일반 컬럼과 집계함수를 같이 쓸 수 없음 --
+-- 일반 컬럼과 집계함수를 같이 쓸 수 없음 
 select ename, sal, (select avg(sal) from emp)
 from emp;
 
@@ -97,7 +92,7 @@ from emp e
 inner join dept d on e.deptno = d.deptno;
 
 
--- 사원명과 해당 사원 부서의 '최대 급여'를 함께 출력   -- 밖의 e를 쓸 수 있?
+-- 사원명과 해당 사원 부서의 '최대 급여'를 함께 출력   
 select e.ename, e.deptno, (select max(sal)
 						   from emp e2 
                            where e2.deptno = e.deptno)
@@ -126,18 +121,64 @@ WHERE e.hiredate >= '1981-01-01' AND e.hiredate <  '1982-01-01'
 and e.sal >= 2000;
 		
 
+
 -- 각 부서에서 가장 먼저 입사한 사원의 정보를 출력
+select e.ename, e.hiredate, e.deptno
+from emp e
+where (e.hiredate, e.deptno) in (select min(hiredate), deptno
+								 from emp
+                                 group by deptno);
 
         
-
 -- 사원 정보에 ename과 더불어 부서 위치(LOC) 출력
+select e.ename `사원명`, (select d.loc
+				 from dept d
+                 where e.deptno = d.deptno) `부서 위치`
+from emp e;
 
+ 
 -- 부서별 인원수와 평균 급여를 가진 가상 테이블을 만들고, 인원수가 5명 이상인 부서만 조회
+select e.dept `부서` , e.number `인원수` , e.average `평균 급여`
+from (select deptno as dept, count(*) as number, avg(sal) as average
+	  from emp 
+	  group by deptno) e
+where e.number >= 5;
 
--- 직급이 'MANAGER'인 사원들이 속한 부서의 모든 사원을 출력
 
--- 30번 부서의 그 누구보다도 급여를 많이 받는 사원 출력
+
+-- 직급이 'MANAGER'인 사원들이 속한 부서의 모든 사원 정보를 출력 
+select e.ename, e.empno, e.deptno 
+from emp e
+where e.deptno in (select deptno
+				   from emp
+                   where job = 'MANAGER');
+
+
+-- 30번 부서에서 급여를 많이 받는 사원 정보 출력
+select e.ename, e.empno, e.deptno, e.sal
+from emp e
+where e.sal in (select max(sal)
+				from emp
+                where deptno = 30);
+
 
 -- 부하 직원이 한 명이라도 있는 '관리자' 사원들의 이름을 출력   - 실무에서 유용
+-- 서브 쿼리 where절에 
+select e.ename
+from emp e
+where e.empno in (select mgr
+				  from emp
+                  WHERE mgr IS NOT NULL);
+                  
+select e.ename
+from emp e
+where exists (select 1
+			  from emp p
+		      where e.empno = p.MGR);
 
 
+-- 조인
+select distinct manager.ename
+from emp manager
+join emp staff
+on manager.empno = staff.MGR;
