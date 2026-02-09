f1c9271b0f269d73d8485df7929ada837ac638c9
edbdmss <edbdms@naver.com>
2026-02-09 22:27:19 +0900

    complete DDL exercise

--- 02_DB_SQL/DDL Exercise.sql
+++ 02_DB_SQL/DDL Exercise.sql
@@ -22,7 +22,8 @@ alter table dept_test add budget int;
 delete from dept_test where deptno = 50;
 
 
--- 
+
+
 -- [시나리오 2] 인사 시스템 데이터 정제 (응용)
 -- 1. 복사 생성: emp 테이블에서 부서번호 20, 30번인 사원의 empno, ename, sal, deptno로 emp_senior 생성
 create table emp_senior as (select empno, ename, sal, deptno from emp);
@@ -34,7 +35,6 @@ alter table emp_senior add constraint PK primary key (empno);
 update emp_senior set sal = sal * 1.15
 where deptno = 20;
 
-
 -- 4. 조건 삭제: 급여(sal)가 2000 미만인 사원 데이터 삭제
 delete from emp_senior where sal < 2000;
 
@@ -43,7 +43,8 @@ delete from emp_senior;
 
 
 
--- 
+
+
 -- [시나리오 3] 서비스 회원 관리 시스템
 -- 1. 테이블 생성: users 
 --    (id: 숫자형, PK, 자동증가 / username: 문자형 20자, 중복불가, 필수 / 
@@ -57,13 +58,19 @@ create table users (
 
 -- 2. 데이터 입력: 'admin', 'admin@test.com' / 'guest', 'guest@test.com' 두 개 행 추가
 --    (id와 reg_date는 자동 입력되도록 설정)
+insert into users(username, email) values('admin', 'admin@test.com'), ('guest', 'guest@test.com');
 
 -- 3. 컬럼 수정: 유저의 상태를 나타내는 status 컬럼(문자형 10자, 기본값 'ACTIVE') 추가
+alter table users add status VARCHAR(10) default 'ACTIVE';
 
 -- 4. 제약 조건 추가: email 컬럼이 NULL이 되지 않도록(NOT NULL) 제약 조건 수정
+alter table users modify email varchar(50) NOT NULL;
 
 -- 5. 데이터 수정: 'guest' 사용자의 상태(status)를 'INACTIVE'로 변경
+update users set status='INACTIVE' where username='guest';
 
 -- 6. 데이터 삭제: 가입일(reg_date)이 오늘 이전인 데이터 중 상태가 'INACTIVE'인 사용자 삭제
+delete from users where reg_date < current_timestamp() and status = 'INACTIVE' AND id IS NOT NULL;
 
 -- 7. 테이블 삭제: users 테이블을 데이터베이스에서 완전히 제거
+drop table users;
