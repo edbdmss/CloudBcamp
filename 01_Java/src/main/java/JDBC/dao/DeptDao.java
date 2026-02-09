a11921a87ede612825340f40ccbd8334577cacbd
edbdmss <edbdms@naver.com>
2026-02-09 13:03:04 +0900

    DB connection

--- 01_Java/src/main/java/JDBC/dao/DeptDao.java
+++ 01_Java/src/main/java/JDBC/dao/DeptDao.java
@@ -1,6 +1,53 @@
 package JDBC.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+import JDBC.dto.DeptDto;
+
 
 public class DeptDao {
-
+	
+	// step01
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");      // mysql이 갖고있는 고유한 driver
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// 모든 부서 정보 검색 메소드
+	// SELECT * FROM dept
+	public static List<DeptDto> getAllDepts() throws SQLException {
+		List<DeptDto> deptList = null;
+		Connection con = null;
+		Statement stmt = null;
+		
+		try {
+			// step02
+			String url = "jdbc:mysql://localhost:3306/scott?serverTimezone=UTC";
+			con = DriverManager.getConnection(url, "root", "edbdms1012");
+			// step03
+			stmt = con.createStatement();
+			// step04
+			stmt.executeQuery("SELECT * FROM dept");
+			
+			System.out.println(con);
+			
+		} finally {
+		}
+		
+		
+		return deptList;
+	}
+	
+	
+	
+	// 부서번호로 특정 부서 정보 검색 메소드
+	
+	
 }
--- 01_Java/src/main/java/JDBC/test/DeptTest.java
+++ 01_Java/src/main/java/JDBC/test/DeptTest.java
@@ -1,9 +1,16 @@
 package JDBC.test;
 
+import JDBC.dao.DeptDao;
+import JDBC.dto.DeptDto;
+
 
 // main 메서드 위치
 public class DeptTest {
     public static void main(String[] args) {
-        
+        try {
+            DeptDao.getAllDepts();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
